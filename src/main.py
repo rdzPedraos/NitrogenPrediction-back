80d75d2dc0e8a9a30f35a548e40671539b6b0647
rdzPedraos <rdzpedraos@gmail.com>
2024-10-14 21:49:04 -0500

    init proyect

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__);
+
+@app.route('/up', methods=['GET'])
+def up():
+    return jsonify({"status": "upa"})
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
