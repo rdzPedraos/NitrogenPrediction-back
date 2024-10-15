0d911be1d890b2a272b84aae28078db51996feb6
rdzPedraos <rdzpedraos@gmail.com>
2024-10-14 21:55:42 -0500

    fix: status = up

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__);
 
 @app.route('/up', methods=['GET'])
 def up():
-    return jsonify({"status": "upa"})
+    return jsonify({"status": "up"})
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
