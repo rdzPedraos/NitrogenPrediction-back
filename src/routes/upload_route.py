9bd4cd25a81a4f7a377f1f108ae2d04274665521
rdzPedraos <rdzpedraos@gmail.com>
2024-10-17 20:41:41 -0500

    refactor: move logic for upload files

--- src/routes/upload_route.py
+++ src/routes/upload_route.py
@@ -1,21 +1,23 @@
+from extensions import logger
+
 import uuid
 from flask import request, jsonify
-from utils.file_manager import save
+from utils.file_manager import saveBandFile, FILE_TYPES
 
 from . import main_blueprint
 
 @main_blueprint.route('/upload-images', methods=['POST'])
 def upload_images():
-    if "bands" not in request.files or "panels" not in request.files:
-        return jsonify({'message': 'Invalid request'}), 400
+    files = request.files
+
+    if(FILE_TYPES['BANDS'] not in files or FILE_TYPES['PANELS'] not in files):
+        return jsonify({'message': 'Invalid files'}), 400
 
     session_id = str(uuid.uuid4())
-    bands = request.files.getlist('bands')
-    panels = request.files.getlist('panels')
 
     # Guardar archivos subidos
-    for files, key in zip([bands, panels], ['bands', 'panels']):
-        for id in range(len(files)):
-            save(session_id, files[id], f"{key}_{str(id+1)}.tif")
+    for type in FILE_TYPES.values():
+        for i, file in enumerate(files.getlist(type)):
+            saveBandFile(file, session_id, type, i)
 
     return jsonify({'session_id': session_id}), 200
