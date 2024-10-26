6014b1185df7025bab02e57236da27b4b979c6e7
rdzPedraos <rdzpedraos@gmail.com>
2024-10-25 23:11:36 -0500

    feat: add cors for allow request from any ip

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from routes import main_blueprint
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Registrar Blueprint
 app.register_blueprint(main_blueprint)
